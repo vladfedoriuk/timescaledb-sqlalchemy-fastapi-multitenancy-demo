dbd6fe9b5f30d72f18c16ecb4529fe024d32cb15
Vladyslav Fedoriuk <vladyslav.fedoriuk@deployed.pl>
2023-11-17 10:02:15 +0100

    Remove the :return: annotations

--- backend/main.py
+++ backend/main.py
@@ -3,16 +3,19 @@ The main file of the backend.
 
 It contains the FastAPI application.
 """
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root() -> dict[str, str]:
-    """
-    Return a message.
-
-    :return: A message.
-    """
+    """Return a 'Hello World' message."""
     return {"message": "Hello World"}
+
+
+@app.post("/sensors-data")
+async def sensors_data(request: Request) -> Response:
+    """Ingest sensors data."""
+    print(await request.json())
+    return Response(status_code=200)
